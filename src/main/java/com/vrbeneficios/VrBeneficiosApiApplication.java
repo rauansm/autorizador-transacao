8357fd93fda8e287d059de56150632e23336c00f
Rauan Santos <rauanmor@gmail.com>
2024-02-07 15:55:18 -0300

    inicia projeto

---
+++ src/main/java/com/vrbeneficios/VrBeneficiosApiApplication.java
@@ -0,0 +1,13 @@
+package com.vrbeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrBeneficiosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrBeneficiosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrbeneficios/VrBeneficiosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrbeneficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrBeneficiosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
